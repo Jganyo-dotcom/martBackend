c9c2f7cac482703ca99a12ce9f235e96353ab288
Jganyo-dotcom <elikemejay@gmail.com>
2026-05-20 18:46:03 +0000

    COOKIE BUG

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,20 @@
 const authmiddleware = (req, res, next) => {
   const token = req.cookies.authToken?.replace(/^"|"$/g, "");
+  console.log("Raw cookie value:", req.cookies.authToken);
+  console.log("First char:", req.cookies.authToken.charAt(0));
+  console.log(
+    "Last char:",
+    req.cookies.authToken.charAt(req.cookies.authToken.length - 1),
+  );
 
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    console.log("token in");
-    console.log(token);
-    console.log(req.cookies);
+
     next();
   } catch (err) {
-    console.log("no token");
-    console.log(token);
-    console.log(req.cookies);
     return res.status(403).json({ message: "Invalid token" });
   }
 };
